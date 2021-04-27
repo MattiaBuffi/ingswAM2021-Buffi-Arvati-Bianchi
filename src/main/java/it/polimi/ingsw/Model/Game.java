b8343935f98a518418cdf95b2822049ae8ff753f
Emanuele <10582228@polimi.it>
2021-04-27 12:54:04 +0200

    merge preparation

--- src/main/java/it/polimi/ingsw/Model/Game.java
+++ src/main/java/it/polimi/ingsw/Model/Game.java
@@ -1,15 +1,14 @@
 package it.polimi.ingsw.Model;
 
-import it.polimi.ingsw.Model.CardMarket.CardMarket;
-import it.polimi.ingsw.Model.Player.Player;
+
 import it.polimi.ingsw.Model.VaticanRoute.VaticanRoute;
 
 import java.util.List;
 
 public class Game {
 
-    private List<Player> player;
-    private CardMarket cardMarket;
+    //private List<Player> player;
+    //private CardMarket cardMarket;
     private ResourceMarket resourceMarket;
     private VaticanRoute vaticanRoute;
 
--- src/main/java/it/polimi/ingsw/Model/Marble/Marble.java
+++
@@ -1,9 +0,0 @@
-package it.polimi.ingsw.Model.Marble;
-
-public abstract class Marble {
-
-    public abstract MarbleColor getColor();
-
-    public abstract void accept(MarbleVisitor visitor);
-
-}
